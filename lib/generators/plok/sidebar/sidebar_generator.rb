1b5f50ebaebf8d802a1c18392089c704187cc688
davelens <github@davelens.be>
2022-05-20 17:42:24 +0200

    Add the basics for a `rails g plok:sidebar` command.

---
+++ lib/generators/plok/sidebar/sidebar_generator.rb
@@ -0,0 +1,27 @@
+require 'rails/generators/base'
+
+class Plok::SidebarGenerator < Rails::Generators::Base
+  source_root File.expand_path('templates', __dir__)
+
+  def copy_initializer_file
+    copy_sidebar_files('wrapper', 'menu_item', 'offcanvas_menu')
+    gsub_file sidebar_file_path('wrapper'), '[brand_name]', app_name
+    gsub_file sidebar_file_path('offcanvas_menu'), '[brand_name]', app_name
+  end
+
+  private
+
+  def app_name
+    Rails.application.class.name.split('::').first
+  end
+
+  def copy_sidebar_files(*partials)
+    partials.each do |partial_name|
+      copy_file "_#{partial_name}.html.erb", sidebar_file_path(partial_name)
+    end
+  end
+
+  def sidebar_file_path(partial_name)
+    "app/views/backend/bs5/sidebar/_#{partial_name}.html.erb"
+  end
+end
---
+++ spec/generator/plok/plok_sidebar_generator_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+describe "Plok::Sidebar", type: :generator do
+end
