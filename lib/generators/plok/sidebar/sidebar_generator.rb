344d12aa66bfc8a2695edbecb378b39d45f85945
davelens <github@davelens.be>
2022-05-21 17:20:30 +0200

    Add the sidebar stylesheets, and installation options through the generator.

--- lib/generators/plok/sidebar/sidebar_generator.rb
+++ lib/generators/plok/sidebar/sidebar_generator.rb
@@ -3,10 +3,20 @@ require 'rails/generators/base'
 class Plok::SidebarGenerator < Rails::Generators::Base
   source_root File.expand_path('templates', __dir__)
 
-  def copy_initializer_file
+  def install
     copy_sidebar_files('wrapper', 'menu_item', 'offcanvas_menu')
     gsub_file sidebar_partial_path('wrapper'), '[brand_name]', app_name
     gsub_file sidebar_partial_path('offcanvas_menu'), '[brand_name]', app_name
+
+    if application_scss_file
+      append_to_file application_scss_file, "@import 'plok/sidebar';\n"
+      append_to_file application_scss_file, "@import 'plok/sidebar_compact';\n"
+    else
+      say("WARNING: No suitable application.scss file found.\n")
+      say("Please add the following imports to your backend application.scss file:\n\n")
+      say("@import 'plok/sidebar';\n")
+      say("@import 'plok/sidebar_compact';\n")
+    end
   end
 
   private
@@ -23,5 +33,15 @@ class Plok::SidebarGenerator < Rails::Generators::Base
 
   def sidebar_partial_path(partial_name)
     "app/views/backend/bs5/sidebar/_#{partial_name}.html.erb"
+  end
+
+  def application_scss_file
+    if File.exists?('app/assets/stylesheets/backend/bs5/application.scss')
+      return 'app/assets/stylesheets/backend/bs5/application.scss'
+    end
+
+    if File.exists?('app/assets/stylesheets/backend/application.scss')
+      return 'app/assets/stylesheets/backend/application.scss'
+    end
   end
 end
