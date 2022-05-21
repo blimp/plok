d958fa3bf84628357188b07fa742cf2018d7dde3
davelens <github@davelens.be>
2022-05-21 16:33:13 +0200

    Rename method sidebar_file_path to sidebar_partial_path.

--- lib/generators/plok/sidebar/sidebar_generator.rb
+++ lib/generators/plok/sidebar/sidebar_generator.rb
@@ -5,8 +5,8 @@ class Plok::SidebarGenerator < Rails::Generators::Base
 
   def copy_initializer_file
     copy_sidebar_files('wrapper', 'menu_item', 'offcanvas_menu')
-    gsub_file sidebar_file_path('wrapper'), '[brand_name]', app_name
-    gsub_file sidebar_file_path('offcanvas_menu'), '[brand_name]', app_name
+    gsub_file sidebar_partial_path('wrapper'), '[brand_name]', app_name
+    gsub_file sidebar_partial_path('offcanvas_menu'), '[brand_name]', app_name
   end
 
   private
@@ -17,11 +17,11 @@ class Plok::SidebarGenerator < Rails::Generators::Base
 
   def copy_sidebar_files(*partials)
     partials.each do |partial_name|
-      copy_file "_#{partial_name}.html.erb", sidebar_file_path(partial_name)
+      copy_file "_#{partial_name}.html.erb", sidebar_partial_path(partial_name)
     end
   end
 
-  def sidebar_file_path(partial_name)
+  def sidebar_partial_path(partial_name)
     "app/views/backend/bs5/sidebar/_#{partial_name}.html.erb"
   end
 end
