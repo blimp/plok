b7e2f672a75b3bd7171b4f99ca08e0e1438adece
davelens <github@davelens.be>
2022-10-07 19:19:56 +0200

    Fix alignment.

--- app/models/search_module.rb
+++ app/models/search_module.rb
@@ -4,7 +4,8 @@ class SearchModule < ActiveRecord::Base
   validates :name, presence: true
 
   def indices
-    SearchIndex.joins('INNER JOIN search_modules ON search_indices.searchable_type = search_modules.name')
-               .where('search_modules.name = ?', name)
+    SearchIndex
+      .joins('INNER JOIN search_modules ON search_indices.searchable_type = search_modules.name')
+      .where('search_modules.name = ?', name)
   end
 end
